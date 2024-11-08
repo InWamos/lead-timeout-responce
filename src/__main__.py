18ef24d317a69b05271281369a2c735c701c8a46
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-08 23:30:41 +0200

    basic compose()

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 
+from pyrogram.sync import compose
 from pyrogram.client import Client
 from dotenv import load_dotenv
 
@@ -12,8 +13,17 @@ if not API_ID or not API_HASH:
     raise ValueError("No config data provided")
 
 async def main() -> None:
-    async with Client("my_account", str(API_ID), str(API_HASH)) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
+
+    apps = [
+        Client("my_account2"),
+        Client("my_account")
+    ]
+
+    for app in apps:
+        app.add_handler
+        
+    await compose(apps)
+
 
 
 if __name__ == "__main__":
