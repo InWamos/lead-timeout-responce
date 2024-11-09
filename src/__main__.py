269dad06361a0bd3a45e655c6b3f8d37f493bd9e
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-09 20:55:04 +0200

    Move session folder to data/sessions and add session auto-launch

--- src/__main__.py
+++ src/__main__.py
@@ -1,27 +1,23 @@
 import asyncio
-import os
 import uvloop
 
+from pathlib import Path
 from pyrogram.sync import compose
 from pyrogram.client import Client
 from pyrogram.handlers.message_handler import MessageHandler
-from dotenv import load_dotenv
 from handlers.timeout_responce import on_message
 
 
-load_dotenv()
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-if not API_ID or not API_HASH:
-    raise ValueError("No config data provided")
-
-
 async def main() -> None:
+    sessions_folder = Path("data/sessions")
+    session_files = list(sessions_folder.glob("*.session"))
 
-    apps = [Client("my_account"), Client("my_account2")]
-
-    for app in apps:
+    apps = []
+    for session_file in session_files:
+        app = Client(str(session_file.stem), workdir="data/sessions/")
         app.add_handler(MessageHandler(on_message))
+        apps.append(app)
+        print(str(session_file.stem))
 
     await compose(apps)
 
