2177eec5fc1eac57580a0b04d476647fbd9b1ed8
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-11 21:29:34 +0200

    Add memory feature for the ones who send command

--- src/handlers/command_responce.py
+++ src/handlers/command_responce.py
@@ -1,5 +1,6 @@
 from pyrogram.types import Message
 from pyrogram.client import Client
+from data_readers.data_reader_io import is_timespan_long_enough, add_client
 
 STICKER_ID = "CAACAgIAAxkBAAKI02cyUrhn_snxmh07Ag7PMsenNowqAAJhAQACEBptIu-IjH2qmk0HNgQ"
 
@@ -7,4 +8,9 @@ async def on_command(client: Client, message: Message) -> None:
 
     if message.chat.type.value == "private" and not message.from_user.is_self and not message.from_user.is_bot:
         if message.text == "работа":
-            await client.send_sticker(chat_id=message.chat.id, sticker=STICKER_ID)
+            client_id = client.name
+            sender_id = str(message.from_user.id)
+        
+            if is_timespan_long_enough(client_id, sender_id):
+                await client.send_sticker(chat_id=message.chat.id, sticker=STICKER_ID)
+                add_client(client_id, sender_id)
