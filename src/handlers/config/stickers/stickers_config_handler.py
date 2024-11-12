47ae2920754662e32fee548280df0390d14ccc57
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-12 21:28:13 +0200

    /get command add

--- src/handlers/config/stickers/stickers_config_handler.py
+++ src/handlers/config/stickers/stickers_config_handler.py
@@ -1,6 +1,6 @@
 from pyrogram.types import Message
 from pyrogram.client import Client
-from data_readers.config.stickers.sticker_config import add_sticker, remove_sticker, get_sticker_config, set_main_sticker
+from data_readers.config.stickers.sticker_config import add_sticker, get_main_sticker, remove_sticker, get_sticker_config, set_main_sticker
 
 async def on_sticker_add_command(client: Client, message: Message) -> None:
     """ Pattern: /add <sticker_name> <sticker_id>
@@ -57,3 +57,31 @@ async def on_sticker_rm_command(client: Client, message: Message) -> None:
         await message.reply(f"Sticker {sticker_name} has been removed")
     except Exception as e:
         await message.reply("Couldn't remove this sticker. Maybe it doesn't exist or you made a typo")
+
+
+async def on_sticker_get_command(client: Client, message: Message) -> None:
+    """Pattern: /get
+
+    Args:
+        client (Client): _description_
+        message (Message): _description_
+    """
+    stickers_config = get_sticker_config()
+    stickers: dict[str, str] = stickers_config["stickers"] # type: ignore
+    try:
+        main_sticker = get_main_sticker()
+    except ValueError as ve:
+        main_sticker = None
+        
+    string_builder = ""
+
+    if stickers:
+        string_builder += "Your sticker setup:\n"
+        for sticker_name, sticker_value in stickers:
+            string_builder += f"{sticker_name} : {sticker_value}\n"
+        if main_sticker:
+            string_builder += f"\nCurrent sticker: {main_sticker}"
+        
+    else: string_builder = "We have no stickers to show. Add them with /add name sticker-id"
+
+    await message.reply(string_builder)
