42bad675840b3a7ab98f15d2e8a093e844b323cb
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-12 17:14:14 +0200

    sticker config data reader

---
+++ src/data_readers/config/stickers/sticker_config.py
@@ -0,0 +1,34 @@
+import json
+
+
+PATH_CONFIG_STICKERS = "data/configs/stickers.json"
+
+
+def remove_sticker(key: str) -> None:
+    sticker_config = get_sticker_config()
+    if key in sticker_config["stickers"]:
+        del sticker_config["stickers"][key]  # type: ignore
+
+
+def add_sticker(key: str, value: str) -> None:
+    sticker_config = get_sticker_config()
+    sticker_config["stickers"][key] = value  # type: ignore
+
+    set_sticker_config(sticker_config)
+
+
+def set_main_sticker(sticker_key: str) -> None:
+    sticker_config = get_sticker_config()
+    sticker_config["chosen_sticker"] = sticker_key
+
+    set_sticker_config(sticker_config)
+
+
+def get_sticker_config() -> dict[str, dict[str, str] | str]:
+    with open(PATH_CONFIG_STICKERS, "r") as json_file:
+        return json.load(json_file)
+
+
+def set_sticker_config(sticker_dict: dict[str, dict[str, str] | str]) -> None:
+    with open(PATH_CONFIG_STICKERS, "w") as json_file:
+        json.dump(sticker_dict, json_file)
