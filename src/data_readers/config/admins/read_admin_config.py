f33f7b1ccbafe64c79bd99de6f2bc24a179203e9
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-12 16:39:33 +0200

    admin config reader

---
+++ src/data_readers/config/admins/read_admin_config.py
@@ -0,0 +1,9 @@
+import json
+
+PATH_CONFIG_STICKERS = "data/configs/stickers.json"
+PATH_CONFIG_ADMINS = "data/configs/admins.json"
+
+
+def get_admins() -> list[str]:
+    with open(PATH_CONFIG_ADMINS, "r") as json_file:
+        return json.load(json_file)
