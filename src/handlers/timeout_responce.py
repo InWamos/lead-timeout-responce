bdfa517660ef7510e71d6074e83b2ce074a93f1d
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-14 02:01:15 +0200

    Get sticker from config file

--- src/handlers/timeout_responce.py
+++ src/handlers/timeout_responce.py
@@ -2,23 +2,27 @@ from datetime import datetime, timedelta
 from pyrogram.types import Message
 from pyrogram.client import Client
 from data_readers.data_reader_io import is_timespan_long_enough, add_client
+from data_readers.config.stickers.sticker_config import (
+    get_main_sticker,
+    get_sticker_id_by_key,
+)
 
-STICKER_ID = "CAACAgQAAxkBAAKH92culXa1_BeNrC5FV9OrNRZ3sft4AAKwFgAC0p4hUdwkJLVYoVIlNgQ"
 
 async def on_message(client: Client, message: Message) -> None:
 
     client_id = client.name
     sender_id = str(message.from_user.id)
-    
-    if is_timespan_long_enough(client_id, sender_id):
 
+    if is_timespan_long_enough(client_id, sender_id):
+        sticker_main_key = get_main_sticker()
+        sticker_main = get_sticker_id_by_key(sticker_main_key)
         first_message_scheduled_date = datetime.now() + timedelta(minutes=3)
         second_message_scheduled_date = datetime.now() + timedelta(minutes=5)
         await message.reply(text="Привет)", schedule_date=first_message_scheduled_date)
         await client.send_sticker(
             chat_id=message.chat.id,
-            sticker=STICKER_ID,
+            sticker=sticker_main,
             schedule_date=second_message_scheduled_date,
         )
-        
+
         add_client(client_id, sender_id)
